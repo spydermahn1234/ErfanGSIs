8d6d9232ac0ae9a8563c739bdfc81bb1bdf61f16
spiderdev123 <92781393+spiderdev123@users.noreply.github.com>
2021-10-29 10:02:04 +0300

    make.sh: rename

--- make.sh
+++ make.sh
@@ -193,7 +193,7 @@ if [ "$outputtype" == "Aonly" ]; then
 fi
 
 date=`date +%Y%m%d`
-outputname="$romtypename-$outputtype-$sourcever-$date-ErfanGSI"
+outputname="$romtypename-$outputtype-$sourcever-$date-NipponGSI"
 outputimagename="$outputname".img
 outputtextname="$outputname".txt
 if [ "$4" == "" ]; then
