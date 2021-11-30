5b374d11a6ff702fed37462496fc7818eb05eed3
spiderdev123 <92781393+spiderdev123@users.noreply.github.com>
2021-11-30 04:44:51 +0200

    prebuilt: fix a weird issue part1

--- prebuilt/12/make.sh
+++ prebuilt/12/make.sh
@@ -7,7 +7,6 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 # Deal with non-flattened apex
 $thispath/../../scripts/apex_extractor.sh $1/apex
 echo "ro.apex.updatable=true" >> $1/product/etc/build.prop
-rm -rf $1/apex/*/
 
 # Copy system files
 rsync -ra $thispath/system/ $systempath
